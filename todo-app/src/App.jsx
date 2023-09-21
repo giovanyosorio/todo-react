a071b88f422104657ceadfd04dfb31e65c52bd5d
giovanyosorio <giovany.torres.799@gmail.com>
2023-09-20 19:50:07 -0500

    Buscando TODOs

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -8,38 +8,41 @@ import React from 'react';
 
 const defaultTodos = [
   { text: "api de flags", completed: true },
-  {  text: "api de books", completed: true  }, 
-    {  text: "api de shopífy", completed: false  },
-    {  text: "api de platzi", completed: false  },
-    {  text: "react", completed: false  }
+  { text: "api de books", completed: true },
+  { text: "api de shopífy", completed: false },
+  { text: "api de platzi", completed: false },
+  { text: "react", completed: false },
+  { text: "NODEjs", completed: false }
 ]
 
 
 
 function App() {
 
-  const [todos,SetTodos]=React.useState(defaultTodos)
-  console.log("los usuarios buscan todos de "+ todos);
-  
-  const [searchValue, setSearchValue]=React.useState("")
-  console.log("los usuarios buscan todos de "+ searchValue);
-    
-  const completedTodos=todos.filter((item)=>!!item.completed).length
-  const total=todos.length
-  
+  const [todos, SetTodos] = React.useState(defaultTodos)
+  const [searchValue, setSearchValue] = React.useState("")
+  const completedTodos = todos.filter((item) => !!item.completed).length
+  const total = todos.length
+  const searchedTodos = todos.filter((item) => { 
+    const todoText=item.text.toLowerCase()
+    const searchText=searchValue.toLocaleLowerCase()
+
+    return todoText.includes(searchText) 
+  })
+  console.log(searchedTodos);
   return (
     <React.Fragment>
-      <TodoCounter completed={completedTodos} total={total}/>
+      <TodoCounter completed={completedTodos} total={total} />
       <TodoSearch searchValue={searchValue}
-      setSearchValue={setSearchValue}/>
+        setSearchValue={setSearchValue} />
 
       <TodoList>
-      {defaultTodos.map((todo)=>
-         <TodoItem key={todo.text}
-         text={todo.text}
-         completed={todo.completed}/>
-      )}
-  
+        {searchedTodos.map((todo) =>
+          <TodoItem key={todo.text}
+            text={todo.text}
+            completed={todo.completed} />
+        )}
+
       </TodoList>
 
       <TodoButton />
